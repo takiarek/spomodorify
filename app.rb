0d9cdbb1dc37286b3a2d324560e0bc44bd368839
Arek Kita <arkadiusz.kita@lunarlogic.io>
2018-06-20 10:14:09 +0200

    Change the step and duration of lowering the volume

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'net/http'
 
 sleep 60*25
 
-100.times do |i|
-  url = URI("https://api.spotify.com/v1/me/player/volume?volume_percent=#{100-i}")
+50.times do |i|
+  url = URI("https://api.spotify.com/v1/me/player/volume?volume_percent=#{100-i*2}")
 
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
@@ -14,5 +14,5 @@ sleep 60*25
   request["Authorization"] = "Bearer #{ENV["SPOTIFY_API_TOKEN"]}"
 
   http.request(request)
-  sleep 2/100
+  sleep 3/50
 end
